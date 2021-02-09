07a1106371ed97ae7f6df679428e8681b058c906
Adrià Sebastia <adria.sebastia@enti.cat>
2021-02-09 08:46:14 +0100

    Protocolo pasar archivo

--- abfp/client.sh
+++ abfp/client.sh
@@ -4,6 +4,8 @@ PORT="2021"
 IP_CLIENT="127.0.0.1"
 IP_SERVER="127.0.0.1"
 
+FILE_NAME="archivo_salida.vaca"
+
 clear
 
 echo "Iniciando cliente del protocolo ABFP"
@@ -18,7 +20,7 @@ RESPONSE=`nc -lp $PORT`
 
 sleep 1
 
-echo "Test Message"
+echo "Test Header"
 
 if [ "$RESPONSE" != "OK_CONN" ]; then
 	echo "ERROR 1: La conexión no se ha producido con el servidor"
@@ -41,5 +43,32 @@ if [ "$RESPONSE" != "YES_IT_IS" ]; then
 	echo "ERROR 2: Handshake no se ha recibido correctamente"
 	exit 2
 fi
+
+echo "(10)Enviar nombre del archivo"
+
+echo "FILE_NAME $FILE_NAME" | nc -q 1 $IP_SERVER $PORT
+
+echo "(11)Listening $PORT"
+
+RESPONSE=`nc -lp $PORT`
+
+sleep 1
+
+echo "Test File Name"
+
+if [ "$RESPONSE" != "OK_FILE_NAME" ]; then
+	echo "ERROR 3: File Name no se ha recibido correctamente"
+	exit 3
+fi
+
+echo "(14)Enviar File Data"
+
+DATA=`cat $FILE_NAME`
+
+echo "$DATA" | nc -q 1 $IP_SERVER $PORT
+
+echo "(15)Listening $PORT"
+
+sleep 1
 
 exit 0
--- abfp/server.sh
+++ abfp/server.sh
@@ -16,16 +16,19 @@ sleep 1
 PREFIX=`echo $HEADER | cut -d " " -f 1`
 IP_CLIENT=`echo $HEADER | cut -d " " -f 2`
 
-echo "(4)Comprobar mensaje"
+echo "Comprobar header"
 
 if [ "$PREFIX" != "ABFP" ]; then
 	echo "ERROR 1: No se ha recibido correctamente la IP"
 	echo "KO_CONN" | nc -q 1 $IP $PORT
 	exit 1
-else
-	echo "Todo correcto"
-	echo "OK_CONN" | nc -q 1 $IP $PORT
 fi
+
+echo "(4)Responder Header"
+
+echo "Todo correcto"
+echo "OK_CONN" | nc -q 1 $IP $PORT
+
 
 echo "(5)Escuchando puerto $PORT"
 
@@ -33,20 +36,50 @@ HANDSHAKE=`nc -lp $PORT`
 
 sleep 1
 
-echo "(8)Comprobar y responder Handshake"
+echo "Comprobar"
 
-if [ "$HANDSHAKE" == "THIS_IS_MY_CLASSROOM" ]; then
-	echo "Todo correcto"
-	echo "YES_IT_IS" | nc -q 1 $IP $PORT
-else
+if [ "$HANDSHAKE" != "THIS_IS_MY_CLASSROOM" ]; then
 	echo "ERROR 2: No se ha recibido el Handshake correctamente"
-	echo "ERROR 2" | nc -q 1 $IP $PORT
+	echo "KO_HANDSHAKE" | nc -q 1 $IP $PORT
 	exit 2
 fi
 
+echo "(8)Responder Handshake"
+
+echo "Todo correcto"
+echo "YES_IT_IS" | nc -q 1 $IP $PORT
+
+
 echo "(9)Escuchando puerto $PORT"
 
-#MSG=`nc -lp $PORT`
+MSG=`nc -lp $PORT`
+
+sleep 1
+
+FILE_PREFIX=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+
+echo "Comprobar File Name"
+
+if [ "$FILE_PREFIX" != "FILE_NAME" ]; then
+	echo "ERROR 3: No se ha recibido el File Name correctamente"
+	echo "KO_FILE_NAME" | nc -q 1 $IP $PORT
+	exit 2
+fi
+
+echo "(12)Responder File Name ($FILE_NAME)"
+
+echo "Todo correcto"
+echo "OK_FILE_NAME" | nc -q 1 $IP $PORT
+
+
+echo "(13)Listening $PORT"
+
+#nc -lp $PORT > archivo_entrada.vaca
+
+
+echo "Todo correcto"
+echo "OK_DATA" | nc -q 1 $IP $PORT
 
 sleep 1
 
